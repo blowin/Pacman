204a8cc09a411e6240efafa1ced23ded2fd9b4dd
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2023-01-08 19:19:46 +0300

    Program - Added UserInput.cs

--- Pacman.Console/Program.cs
+++ Pacman.Console/Program.cs
@@ -7,58 +7,19 @@ internal class Program
         System.Console.CursorVisible = false;
         
         var map = new GameMap("map.txt");
-        ConsoleKeyInfo pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
-
-        Task.Run(() =>
-        {
-            while (true)
-                pressedKey = System.Console.ReadKey();
-        });
-
         var pacman = new Pacman(new IntVector2(1, 1));
         var score = new Score(new IntVector2(32, 0));
+        var input = new UserInput(pacman, map, score);
         
         while (true)
         {
             System.Console.Clear();
-            
-            HandleInput(pressedKey, pacman, map, score);
-            
+            input.Update();    
             map.Draw();
             pacman.Draw();
             score.Draw();
             
             Thread.Sleep(1000);
         }
-    }
-    
-    private static void HandleInput(ConsoleKeyInfo pressedKey,  Pacman pacman, GameMap map, Score score)
-    {
-        var directions = GetDirection(pressedKey);
-        var nextPacmanPosition = pacman.Position + directions;
-        
-        if (!map.IsAvailablePointForMovement(nextPacmanPosition)) 
-            return;
-        
-        pacman.MoveTo(nextPacmanPosition);
-        
-        if (!map.IsScore(nextPacmanPosition)) 
-            return;
-        
-        score.Increase();
-        map.EatScore(nextPacmanPosition);
-    }
-    
-    private static IntVector2 GetDirection(ConsoleKeyInfo pressedKey)
-    {
-        if (pressedKey.Key == ConsoleKey.UpArrow)
-            return new IntVector2(0, -1);
-        if (pressedKey.Key == ConsoleKey.DownArrow)
-            return new IntVector2(0, 1);
-        if (pressedKey.Key == ConsoleKey.LeftArrow)
-            return new IntVector2(-1, 0);
-        if (pressedKey.Key == ConsoleKey.RightArrow)
-            return new IntVector2(1, 0);
-        return IntVector2.Zero;
     }
 }
---
+++ Pacman.Console/UserInput.cs
@@ -0,0 +1,52 @@
+﻿namespace Pacman.Console;
+
+public class UserInput
+{
+    private ConsoleKeyInfo _pressedKey;
+    private readonly Pacman _pacman;
+    private readonly GameMap _map;
+    private readonly Score _score;
+
+    public UserInput(Pacman pacman, GameMap map, Score score)
+    {
+        _pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+        _pacman = pacman;
+        _map = map;
+        _score = score;
+            
+        var _ = Task.Factory.StartNew(() =>
+        {
+            while (true)
+                _pressedKey = System.Console.ReadKey();
+        }, TaskCreationOptions.LongRunning);
+    }
+
+    public void Update()
+    {
+        var directions = GetDirection();
+        var nextPacmanPosition = _pacman.Position + directions;
+        
+        if (!_map.IsAvailablePointForMovement(nextPacmanPosition)) 
+            return;
+        
+        _pacman.MoveTo(nextPacmanPosition);
+        
+        if (!_map.IsScore(nextPacmanPosition)) 
+            return;
+        
+        _score.Increase();
+        _map.EatScore(nextPacmanPosition);
+    }
+        
+    private IntVector2 GetDirection()
+    {
+        return _pressedKey.Key switch
+        {
+            ConsoleKey.UpArrow => new IntVector2(0, -1),
+            ConsoleKey.DownArrow => new IntVector2(0, 1),
+            ConsoleKey.LeftArrow => new IntVector2(-1, 0),
+            ConsoleKey.RightArrow => new IntVector2(1, 0),
+            _ => IntVector2.Zero
+        };
+    }
+}
